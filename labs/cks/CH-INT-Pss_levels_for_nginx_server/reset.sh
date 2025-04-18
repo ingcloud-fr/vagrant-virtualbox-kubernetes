f813b7d058bc9f886b5ed807d3c4f2bb4ef28ad8
VincentS <vincent.schultz.fr@gmail.com>
2025-04-18 14:32:58 +0200

    Retrait du dossier labs du suivi Git

--- labs/cks/CH-INT-Pss_levels_for_nginx_server/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🧹 Cleaning up the lab environment..."
-kubectl delete ns team-green --force --ignore-not-found > /dev/null 2>&1
-kubectl delete ns team-orange --force --ignore-not-found > /dev/null 2>&1
-kubectl delete ns team-red --force --ignore-not-found > /dev/null 2>&1
-
-rm -rf ~/manifests
